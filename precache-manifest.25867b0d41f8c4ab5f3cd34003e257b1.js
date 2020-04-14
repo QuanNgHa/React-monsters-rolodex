0ea79b542bed7e216244f1da2a483b836f3cb05f
QuanNgHa <quanngha.dev@gmail.com>
2020-04-14 22:19:24 +0800

    Updates

---
+++ precache-manifest.25867b0d41f8c4ab5f3cd34003e257b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73f761711ddae6d6a984c5da15e9983",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6b5e607c5d3d6d163085",
+    "url": "/react-monsters-rolodex/static/css/main.e9c4a3dd.chunk.css"
+  },
+  {
+    "revision": "abd7d71b0a1f3859dfb8",
+    "url": "/react-monsters-rolodex/static/js/2.0adb66df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.0adb66df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5e607c5d3d6d163085",
+    "url": "/react-monsters-rolodex/static/js/main.a7bd6191.chunk.js"
+  },
+  {
+    "revision": "b2cb901566663f18323e",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.a30671e1.js"
+  }
+]);
